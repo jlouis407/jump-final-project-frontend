667f2145caf2b3912af1aebd630cab6da93144c6
SergioZ96 <sergiod.zurita@gmail.com>
2021-09-14 12:29:43 -0400

    updated Home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,61 +1,54 @@
-import React, {useState} from "react";
+import React, {useState, Component} from "react";
 import Footer from "./Footer";
 import {NavLink} from "react-router-dom";
 
-
-import React, {Component} from "react";
 import "./App.css";
-import {NavLink} from "react-router-dom";
-import axios from "axios";
 import Restaurant from "./Restaurant";
 
 
+const Home = () => {
 
-
-function Home(){
+    
     
 const [restaurants, setRestaurants] = useState([]);
 const [name, setName] = useState("");
 
-    const getRestaurantsByName = () => {
+    const getRestaurants = () => {
+
         fetch('http://localhost:6060/restaurant', {
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json'
-            },
-
-             body: JSON.stringify(name)
-
+            }
         })
         .then(result => result.json())
         .then(data => {
+            console.log(data);
             setRestaurants(data)
         })
+
     }
 
     const handleChange = e =>{
+        
         setName(e.target.value)
-    }
-
-    const handleSubmit = () => {
-        {restaurants.map( restaurant => <Restaurant name = {restaurant.name} description = {restaurant.description}/> )} 
-
     }
 
     return (
         <div>
         <h1 style={{color: "white"}}>Home</h1>
+        <button onClick={getRestaurants}>Get Cars</button>
         <img src="/images/Cognixia.png"/>
              <nav style={{paddingBottom: "100px"}}>
                <NavLink style={{fontSize: "2.5em", color: "white", textDecoration: "underline", margin: "50px"}} exact to="/login" >Log In</NavLink>
                <NavLink style={{fontSize: "2.5em", color: "white", textDecoration: "underline", margin: "50px" }} exact to="/signup" activeClassName="active">Sign Up</NavLink>
             </nav>
-        <form onSubmit={this.handleSubmit}> 
+       {/*  <form onSubmit={getRestaurants}> 
             <label>
                 <input margin="50px" size="30" type="text" name="name" placeholder="Search for a restaurant..." value = {this.state.name} onChange={this.handleChange}/>
             </label>
             <input type="submit" value="Go" />
-        </form>
+        </form> */}
             <Footer class="footer"/>
         </div>
     ); 
